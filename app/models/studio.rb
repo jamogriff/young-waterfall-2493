a7103983f235fa045d58e7e85e1c0c380695470a
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-01 10:38:27 -0600

    Implements list of all studios movies actors on show page

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,8 @@
 class Studio < ApplicationRecord
   has_many :movies
+
+  def select_actors
+    movies.select('actors.*').joins(:actors).where("actors.currently_working = true").order("actors.age").distinct
+    #binding.pry
+  end
 end
--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe 'Studio info page' do
     @marvel = Studio.create!(name: "Marvel Studios", location: "Hollywood")
     @iron_man = @marvel.movies.create!(title: "Iron Man", creation_year: 2003, genre: "Action")
     @avengers = @marvel.movies.create!(title: "The Avengers", creation_year: 2011, genre: "Action")
+    @harrison = Actor.create!(name: "Harrison Ford", age: 71, currently_working: false)
+    @scarlett = Actor.create!(name: "Scarlett Jo", age: 40, currently_working: true)
+    @mike = Actor.create!(name: "Mike Birbiglia", age: 51, currently_working: true)
+    @iron_man.actors << @harrison
+    @iron_man.actors << @mike
+    @avengers.actors << @scarlett
+    @avengers.actors << @mike
   end
 
   describe 'items shown on page' do
@@ -19,6 +26,13 @@ RSpec.describe 'Studio info page' do
       visit "/studios/#{@marvel.id}"
       expect(page).to have_content @iron_man.title
       expect(page).to have_content @avengers.title
+    end
+
+    it 'has list of currently working actors ordered by age' do
+      visit "/studios/#{@marvel.id}"
+      save_and_open_page
+      expect(page).to have_content @scarlett.name
+      expect(page).to have_content @mike.name
     end
   end
 
