ee058866a4739afc3a5f60c7bb022c4d6d1a6c80
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-01 10:02:09 -0600

    Implements helper method in Actor class

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ApplicationRecord
   has_many :common_films
   has_many :movies, through: :common_films
+
+  def self.working_by_age
+    where(currently_working: true).order(:age)
+  end
 end
+
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Actor do
+
+  before :each do
+    Actor.destroy_all
+    @harrison = Actor.create!(name: "Harrison Ford", age: 71, currently_working: false)
+    @scarlett = Actor.create!(name: "Scarlett Jo", age: 40, currently_working: true)
+    @mike = Actor.create!(name: "Mike Birbiglia", age: 51, currently_working: true)
+  end
+
   describe 'relationships' do
     it { should have_many :common_films }
     it { should have_many(:movies).through(:common_films) }
+  end
+
+  describe 'class methods' do
+    it 'should return list of working actors from oldest to youngest' do
+      expect(Actor.working_by_age.first).to eq @scarlett
+      expect(Actor.working_by_age.last).to eq @mike
+      expect(Actor.working_by_age.include?(@harrison)).to eq false
+    end
   end
 end
